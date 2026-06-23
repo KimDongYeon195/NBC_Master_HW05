d3ef4365c041d41af0a2f4c507a9c3e75c3d9c37
Austin\AustinKim <dong961125@gmail.com>
2026-06-24 02:15:19 +0900

    Interface와 Delegate를 활용한 과제 실습
    - OnHealthDamaged를 통해 UI를 업데이트 해보기
    - OnHealthDead로 플레이어가 실제 사망하는 모습을 만들어보기

---
+++ Source/NBC_Master_HW05/NBC_Master_HW05GameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NBC_Master_HW05GameMode.generated.h"
+
+UCLASS(minimalapi)
+class ANBC_Master_HW05GameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ANBC_Master_HW05GameMode();
+};
+
+
+
